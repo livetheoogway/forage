23bf9777a83e9bf1edae83591975652ddc72fab9
Tushar Naik <tushar.knaik@gmail.com>
2026-01-05 00:35:11 +0530

    Updated copyright year

--- forage-models/src/main/java/com/livetheoogway/forage/models/result/field/FieldTypeVisitor.java
+++ forage-models/src/main/java/com/livetheoogway/forage/models/result/field/FieldTypeVisitor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022. Live the Oogway, Tushar Naik
+ * Copyright 2026. Live the Oogway, Tushar Naik
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
